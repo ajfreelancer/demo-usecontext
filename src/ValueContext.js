2f348c537804cd7cbdc5bf81ec1d3cb7d8e8a817
Abdul Jabbar <abdul.jabbar.khalid@gmail.com>
2020-08-28 20:53:49 +0500

    passed useState
    Passed the whole state from App.js to Child.js through CONTEXT to be able to make the change at child level.

--- src/ValueContext.js
+++ src/ValueContext.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-<<<<<<< HEAD
 const ValueContext = React.createContext(45, () => {})
-=======
-const ValueContext = React.createContext(45)
->>>>>>> master
 
 export default ValueContext;
